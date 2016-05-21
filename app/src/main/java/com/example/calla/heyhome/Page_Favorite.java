b3024e83f561751ab24786cdd2224a1438b24646
CallaChang <Calla@Callas-MacBook-Pro.local>
2016-05-21 15:46:33 -0700

    some java file

---
+++ app/src/main/java/com/example/calla/heyhome/Page_Favorite.java
@@ -0,0 +1,21 @@
+package com.example.calla.heyhome;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Page_Favorite extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.activity_page_favorite, container, false);
+        //show the whole page here
+
+
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/example/calla/heyhome/Page_Homepage.java
@@ -0,0 +1,22 @@
+package com.example.calla.heyhome;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Page_Homepage extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.activity_page_homepage, container, false);
+        //show the whole page here
+
+
+        return rootView;
+    }
+}
